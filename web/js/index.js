050bf8e65929ec3962e94dbd062ad40a3dcd5392
David Chiem <davidchiem@Davids-MacBook-Air.local>
2018-05-25 01:34:37 -0500

    frontend development and design of home

---
+++ web/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$('.menu-link').click(function() {
+		$('.menu-link').removeClass('active');
+		$(this).toggleClass('active');
+	});
+
+	$('#register-tabs span').click(function() {
+		$('#register-tabs span, .register-form').removeClass('active');
+		var form_name = $(this).attr('id').split('-')[0];
+		$('#' + form_name + '-tab').addClass('active');
+		$('#' + form_name + '-form').addClass('active');
+	});
+});
